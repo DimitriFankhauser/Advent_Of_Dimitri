8b820eb302cf03cc1de97480bef571a5576949f4
Dimitri.Fankhauser <dimitri.fankhauser@proton.me>
2024-12-04 19:41:24 +0100

    feat: ex06

---
+++ python/exercises/exercise06/ex06.py
@@ -0,0 +1,12 @@
+# let's learn a very important skill:
+# reading files
+# santa has hidden a present in this folder (it's not exactly hidden, okay...)
+# the file is called present.txt
+# Task: read the file and print the entire content to the console :)
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
